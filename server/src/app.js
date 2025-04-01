021236d37de21e3cd0ddd1646ae965ed05f7585d
Yannick <yannick.liebnau@gmail.com>
2025-04-01 07:54:21 +0200

    ready for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -16,6 +16,12 @@ app.use(
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use("/v1", api);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
